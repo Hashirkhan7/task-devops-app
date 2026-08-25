0cfa14651a70b83f30d001938276aabeda1187b1
Hashirkhan7 <hashirkhanhash77@gmail.com>
2026-08-25 17:23:27 +0530

    Add pytest tests for Flask app

---
+++ test_app.py
@@ -0,0 +1,12 @@
+from app import app
+
+def test_home_page():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_get_tasks():
+    client = app.test_client()
+    response = client.get("/tasks")
+    assert response.status_code == 200
+    
